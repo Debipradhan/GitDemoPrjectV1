d90786f52365ff22acd9a1c652235a8ff1cc000a
Nalinakshee <Nalinakshee@Bhabani>
2022-02-02 14:40:55 +0530

    First Commit

---
+++ src/GitDemo/Test1.java
@@ -0,0 +1,19 @@
+package GitDemo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int hungry = 100;
+		
+		if(hungry == 100)
+		{
+			System.out.println("Eat something");
+		}
+		else
+		{
+			System.out.println("Do work......!!!!!");
+		}
+
+	}
+
+}
