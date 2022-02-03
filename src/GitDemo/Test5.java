cec32a50d04a67148d60091f641b307ba61159a8
Nalinakshee <Nalinakshee@Bhabani>
2022-02-03 17:13:33 +0530

    feb2022 i am commit

---
+++ src/GitDemo/Test5.java
@@ -0,0 +1,14 @@
+package GitDemo;
+
+public class Test5 {
+
+	
+	
+	public static void main(String[] args) {
+		String s1="debi";
+		String s2=new String("debi");
+		System.out.println(s1.equals(s2));
+	
+	}
+
+}
